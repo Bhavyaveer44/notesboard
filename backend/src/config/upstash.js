571925d54121bb9ccd48a9bd505ab557026b27c4
Bhavyaveer44 <bhavyaveer@gmail.com>
2025-11-21 16:01:44 +0530

    first proto

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,11 @@
+import {Ratelimit} from "@upstash/ratelimit";
+import {Redis} from "@upstash/redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+//create a rate limit that allows 100 reqs per min
+const ratelimit=new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(100, "60 s"),
+});
+export default ratelimit;
